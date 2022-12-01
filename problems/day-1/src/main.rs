bb726c18e7278737cbbfaf525d9e1b4e8db91b93
Devon Terrell <devont@remitly.com>
2022-12-01 11:32:35 -0800

    Day one, parts 1 and 2

--- problems/day-1/src/main.rs
+++ problems/day-1/src/main.rs
@@ -1,3 +1,57 @@
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Lines},
+    path::Path,
+};
+
+static INPUT_FILE_NAME: &str = "data/day-1/input.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let lines = read_lines(INPUT_FILE_NAME)
+        .expect(&format!("Expect file to be present: {}", INPUT_FILE_NAME));
+    let groups = segment_lines(lines, |s| s.is_empty());
+    let processed: Vec<i32> = groups
+        .iter()
+        .map(|g| {
+            g.iter()
+                .map(|i| {
+                    let value: i32 = i.parse().expect("Should parse");
+                    value
+                })
+                .sum::<i32>()
+        })
+        .sorted()
+        .collect();
+    let max = processed.last();
+    let max_3 = processed.iter().rev().take(3).sum::<i32>();
+    println!("{:#?}", processed);
+    println!("Max: {:?}", max);
+    println!("Max 3: {:?}", max_3);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn segment_lines<F>(lines: Lines<BufReader<File>>, mut cut_criteria: F) -> Vec<Vec<String>>
+where
+    F: FnMut(String) -> bool,
+{
+    let mut groups: Vec<Vec<String>> = vec![];
+    let mut group: Vec<String> = vec![];
+    for line in lines {
+        let text = line.expect("Should be able to read a line");
+        if cut_criteria(text.clone()) {
+            groups.push(group.clone());
+            group = vec![];
+        } else {
+            group.push(text);
+        }
+    }
+    groups
 }
