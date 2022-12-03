8ad86b8781644405df90e813c8834a557905b3b6
Devon <axiomabsolute@gmail.com>
2022-12-03 00:31:20 -0800

    Day 2, part 2; refactor part 1
    Part two required changing how the solution generated groups of text,
    swapping from a line-based method to a multi-line chunking problem. How
    each group of strings is processed from there is largely the same,
    though the number of elements per group also changed.
    
    - Extract the functionality for finding overlaps between an arbitrary
      number of groups of strings
    - Extract the functionality for breaking the source text into groups.
      Least common denominator for input is (Vec<String>) ->
      Vec<Vec<String>>, to allow both line-based and multi-line groupings.

--- problems/day-3/src/main.rs
+++ problems/day-3/src/main.rs
@@ -1,16 +1,56 @@
 use std::collections::HashSet;
-use std::convert::From;
 
 use shared::{read_lines, AoCProblem, AoCSolution, Solution};
 
 fn priority(c: char) -> u32 {
     // Annoyingly the problem puts 'A' after 'a' in the code points, so we have to do some arithmetic to swap them back
+    // a-z is [1, 27)
     let basis = if c.is_ascii_lowercase() {
         ('a' as u32) - 1
     } else {
+        // A-Z is [27, 57)
         ('A' as u32) - 1 - 26
     };
     (c as u32) - basis
+}
+
+fn find_overlap<'a, I>(vals: I) -> Option<char>
+where
+    I: Iterator<Item = &'a String>,
+{
+    vals.map(|s| s.chars().collect::<HashSet<char>>())
+        .reduce(|p, n| {
+            p.intersection(&n)
+                .map(|c| c.clone())
+                .collect::<HashSet<char>>()
+        })
+        .expect("Should reduce")
+        .iter()
+        .take(1)
+        .next()
+        .map(|c| c.to_owned())
+}
+
+fn group_compartments<'a, I>(vals: I) -> Vec<Vec<String>>
+where
+    I: Iterator<Item = &'a String>,
+{
+    vals.map(|line| {
+        let parts = line.split_at(line.len() / 2);
+        vec![parts.0.to_owned(), parts.1.to_owned()]
+    })
+    .collect()
+}
+
+fn group_badges<'a, I>(vals: I) -> Vec<Vec<String>>
+where
+    I: Iterator<Item = &'a String>,
+{
+    vals.map(|s| s.to_owned())
+        .collect::<Vec<String>>()
+        .chunks(3)
+        .map(|chunk| Vec::from(chunk))
+        .collect::<Vec<Vec<String>>>()
 }
 
 struct Day3 {}
@@ -21,24 +61,23 @@ impl AoCProblem for Day3 {
 }
 impl Solution for Day3 {
     fn solution(&self, path: &str) {
-        let shared_items: Vec<char> = read_lines(path)
+        let lines: Vec<String> = read_lines(path)
             .expect("Should be able to read data")
-            .map(|line| {
-                let text = line.expect("Should be able to read line");
-                let parts = text.split_at(text.len() / 2);
-                let left = parts.0.chars().collect::<HashSet<char>>();
-                let right = parts.1.chars().collect::<HashSet<char>>();
-                left.intersection(&right)
-                    .take(1)
-                    .next()
-                    .expect("There should be a single character shared")
-                    .clone()
-            })
+            .map(|line| line.expect("Should be able to read line"))
             .collect();
-        println!(
-            "{:#?}",
-            shared_items.iter().map(|i| priority(*i)).sum::<u32>()
-        );
+        let compartments = group_compartments(lines.iter());
+        let shared_item_prioities = compartments
+            .iter()
+            .map(|c| find_overlap(c.iter()))
+            .map(|c| priority(c.expect("Should be an overlap")));
+        println!("{:#?}", shared_item_prioities.sum::<u32>());
+
+        let badge_groups = group_badges(lines.iter());
+        let badge_priorities = badge_groups
+            .iter()
+            .map(|g| find_overlap(g.iter()))
+            .map(|c| priority(c.expect("Should be an overlap")));
+        println!("{:#?}", badge_priorities.sum::<u32>());
     }
 }
 
