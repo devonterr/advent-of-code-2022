7f8e3d182fad9c52a21c5106f9dd15de5d2b41c4
Devon Terrell <devont@remitly.com>
2022-12-14 16:17:38 -0800

    Day 12, part 2

--- problems/day-12/src/main.rs
+++ problems/day-12/src/main.rs
@@ -1,53 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use shared::{read_lines, AoCProblem, AoCSolution, Solution};
-
-// fn find_shortest_path(
-//     nodes: &Grid,
-//     edges: &Vec<Edge>,
-//     visited: HashSet<Coordinate>,
-//     // cost_cache: HashMap<Coordinate, usize>,
-//     path: Vec<Coordinate>,
-//     from: Coordinate,
-//     to: &Coordinate,
-// ) -> Option<Vec<Coordinate>> {
-//     // If we're at the end ,return so_far
-//     if from == *to {
-//         return Some(path);
-//     }
-//     // Otherwrise the shortest path is the minimum of the shortest paths from each unvisited neighbor to the end
-//     nodes
-//         .neighbors(from.0, from.1)
-//         .into_iter()
-//         .filter(|n| !visited.contains(n))
-//         .filter(|n| {
-//             edges
-//                 .iter()
-//                 .filter(|e| e.from == from && e.to == to)
-//                 .next()
-//                 .is_some()
-//         })
-//         .map(|n| {
-//             let mut new_visited = visited.clone();
-//             new_visited.insert(n.clone());
-//             let mut new_path = path.clone();
-//             new_path.push(n.clone());
-//             find_shortest_path(nodes, edges, new_visited, new_path, n, &to)
-//         })
-//         .filter(|o| o.is_some())
-//         .map(|o| o.unwrap())
-//         .min_by_key(|p| p.len())
-// }
 
 // (x, y)
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 struct Coordinate(usize, usize);
-
-#[derive(Clone, Debug)]
-struct Edge {
-    from: Coordinate,
-    to: Coordinate,
-}
 
 fn can_reach(from: char, to: char) -> bool {
     let normalized_from = if from == 'S' { 'a' } else { from };
@@ -60,137 +17,129 @@ struct Grid {
     nodes: Vec<Vec<char>>,
 }
 impl Grid {
-    fn bounds(&self) -> (usize, usize) {
-        let max_y = self.nodes.len();
-        let max_x = self.nodes[0].len();
-        (max_x, max_y)
+    fn get(&self, a: Coordinate) -> char {
+        self.nodes[a.1][a.0]
     }
-    fn neighbors(&self, x: usize, y: usize) -> Vec<Coordinate> {
-        let (max_x, max_y) = self.bounds();
-        let mut results = vec![(x + 1, y), (x, y + 1)]
-            .iter()
-            .filter(|(x, y)| x >= &0 && y >= &0 && x < &max_x && y < &max_y)
-            .map(|c| c.to_owned())
-            .map(|c| Coordinate(c.0, c.1))
-            .collect::<Vec<Coordinate>>();
-        if x > 0 {
-            results.push(Coordinate(x - 1, y));
+
+    fn find_first(&self, value: char) -> Option<Coordinate> {
+        for y in 0..self.nodes.len() {
+            for x in 0..self.nodes[0].len() {
+                if self.nodes[y][x] == value {
+                    return Some(Coordinate(x, y));
+                }
+            }
         }
-        if y > 0 {
-            results.push(Coordinate(x, y - 1));
+        None
+    }
+    fn find_start(&self) -> Option<Coordinate> {
+        self.find_first('S')
+    }
+    fn find_end(&self) -> Option<Coordinate> {
+        self.find_first('E')
+    }
+    fn find_all<T>(&self, predicate: T) -> Vec<Coordinate>
+    where
+        T: Fn(char) -> bool,
+    {
+        let mut results = vec![];
+        for y in 0..self.nodes.len() {
+            for x in 0..self.nodes[0].len() {
+                if predicate(self.nodes[y][x]) {
+                    results.push(Coordinate(x, y))
+                }
+            }
         }
         results
     }
+    fn find_starts(&self) -> Vec<Coordinate> {
+        self.find_all(|v| v == 'S' || v == 'a')
+    }
 
-    fn iter_indexes(&self) -> Vec<Coordinate> {
-        let mut result = vec![];
-        for y in 0..self.nodes.len() {
-            for x in 0..self.nodes[0].len() {
-                let c = Coordinate(x, y);
-                result.push(c);
-            }
+    fn one_step_condition(&self, start: &Coordinate, end: &Coordinate) -> bool {
+        let start_value = self.get(start.clone());
+        let end_value = self.get(end.clone());
+        let normalized_start_value = if start_value == 'S' { 'a' } else { start_value };
+        let normalized_end_value = if end_value == 'E' { 'z' } else { end_value };
+        can_reach(normalized_start_value, normalized_end_value)
+    }
+
+    fn in_bounds(&self, c: Coordinate) -> bool {
+        let Coordinate(x, y) = c;
+        x >= 0 && y >= 0 && y < self.nodes.len() && x < self.nodes[0].len()
+    }
+
+    fn neighbors<T>(
+        &self,
+        from: Coordinate,
+        visited: &HashSet<Coordinate>,
+        predicate: &T,
+    ) -> Vec<Coordinate>
+    where
+        T: Fn(&Coordinate, &Coordinate) -> bool,
+    {
+        // Valid neighbors are neighboring indexes which are in bounds, have not been visited yet, and satisfy the predicate
+        let Coordinate(x, y) = from;
+        let mut candidates = vec![Coordinate(x + 1, y), Coordinate(x, y + 1)];
+        if x > 0 {
+            candidates.push(Coordinate(x - 1, y));
         }
-        result
-    }
-
-    fn get(&self, coordinate: Coordinate) -> char {
-        self.nodes[coordinate.1][coordinate.0]
-    }
-
-    fn start(&self) -> Coordinate {
-        self.iter_indexes()
+        if y > 0 {
+            candidates.push(Coordinate(x, y - 1));
+        }
+        candidates
             .into_iter()
-            .filter(|i| self.get(i.to_owned()) == 'S')
-            .next()
-            .expect("Should have a start")
+            .filter(|c| self.in_bounds(c.to_owned()) && !visited.contains(c) && predicate(&from, c))
+            .collect()
     }
 
-    fn end(&self) -> Coordinate {
-        self.iter_indexes()
-            .into_iter()
-            .filter(|i| self.get(i.to_owned()) == 'E')
-            .next()
-            .expect("Should have a start")
-    }
-    fn starts(&self) -> Vec<Coordinate> {
-        self.iter_indexes()
-            .into_iter()
-            .filter(|i| {
-                let value = self.get(i.to_owned());
-                value == 'S' || value == 'a'
-            })
-            .collect::<Vec<Coordinate>>()
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Graph {
-    nodes: Grid,
-    edges: Vec<Edge>,
-}
-impl Graph {
-    fn one_up_graph(grid: Grid) -> Graph {
-        let edges = grid
-            .iter_indexes()
-            .iter()
-            .flat_map(|c| {
-                grid.neighbors(c.0.clone(), c.1.clone())
-                    .iter()
-                    .map(|n| Edge {
-                        from: c.clone(),
-                        to: Coordinate(n.0, n.1).clone(),
-                    })
-                    .collect::<Vec<Edge>>()
-            })
-            .filter(|candidate_edge| {
-                can_reach(
-                    grid.get(candidate_edge.from.clone()),
-                    grid.get(candidate_edge.to.clone()),
-                )
-            })
-            .collect();
-        Graph { nodes: grid, edges }
-    }
-
-    fn find_shortest_path(&self) -> Option<Vec<Coordinate>> {
-        self.find_shortest_path_from(self.nodes.start())
-    }
-
-    fn find_shortest_path_from(&self, start: Coordinate) -> Option<Vec<Coordinate>> {
-        let end = self.nodes.end();
-        let mut candidate_paths: Vec<(usize, Vec<Coordinate>)> = vec![(0, vec![start])];
+    fn shortest_paths<T>(
+        &self,
+        starts: Vec<Coordinate>,
+        end: Coordinate,
+        condition: T,
+    ) -> HashMap<Coordinate, usize>
+    where
+        T: Fn(&Coordinate, &Coordinate) -> bool,
+    {
         let mut visited: HashSet<Coordinate> = HashSet::new();
-
-        loop {
-            // Ensure the lowest weight is at the end
-            candidate_paths.sort_by_key(|e| e.0);
-            candidate_paths.reverse();
-
-            // Pick the lightest-weight path, from the end
-            if candidate_paths.is_empty() {
-                return None;
-            }
-            let (next_to_consider_weight, next_to_consider_path) =
-                candidate_paths.pop().expect("Should be non-empty");
-            let next_to_consider_tail = &next_to_consider_path[next_to_consider_path.len() - 1];
-            if *next_to_consider_tail == end {
-                // Drop the first item, the start of the path
-                return Some(next_to_consider_path.to_owned()[1..].to_vec());
-            }
-            let next_candidate_nodes = self
-                .edges
-                .iter()
-                .filter(|edge| edge.from == next_to_consider_tail.clone())
-                .map(|e| e.to.clone())
-                .filter(|c| !visited.contains(c))
-                .collect::<Vec<Coordinate>>();
-            for ncn in next_candidate_nodes {
-                visited.insert(ncn.clone());
-                let mut new_path = next_to_consider_path.clone();
-                new_path.push(ncn);
-                candidate_paths.push((next_to_consider_weight.clone() + 1, new_path));
-            }
+        let mut results: HashMap<Coordinate, Vec<Coordinate>> = HashMap::new();
+        let mut candidates: Vec<Vec<Coordinate>> = vec![];
+        for start in starts {
+            candidates.push(vec![start]);
         }
+        let mut is_empty: bool = candidates.is_empty();
+        while !is_empty {
+            let mut next_candidates: Vec<Vec<Coordinate>> = vec![];
+            for i in 0..candidates.len() {
+                let candidate = &candidates[i];
+                if results.contains_key(&candidate[0]) {
+                    continue;
+                }
+                if candidate[candidate.len() - 1] == end {
+                    results.insert(candidate[0].clone(), candidate.clone());
+                } else {
+                    let candidate_neighbors = self.neighbors(
+                        candidate[candidate.len() - 1].clone(),
+                        &visited,
+                        &condition,
+                    );
+                    for candidate_neighbor in candidate_neighbors {
+                        let mut next_candidate = vec![];
+                        next_candidate.extend(candidate.clone());
+                        next_candidate.push(candidate_neighbor.clone());
+                        visited.insert(candidate_neighbor.clone());
+                        next_candidates.push(next_candidate);
+                    }
+                }
+            }
+            candidates.clear();
+            candidates.extend(next_candidates);
+            is_empty = candidates.is_empty();
+        }
+        results
+            .iter()
+            .map(|kv| (kv.0.clone(), kv.1.len() - 1))
+            .collect::<HashMap<Coordinate, usize>>()
     }
 }
 
@@ -209,45 +158,23 @@ impl Solution for Day12 {
             .collect::<Vec<Vec<char>>>();
 
         let grid = Grid { nodes: lines };
-        let bounds = grid.bounds();
-        let graph = Graph::one_up_graph(grid);
-        let path = graph.find_shortest_path().expect("Should have an answer");
-        println!("part one: {:#?}", path.len());
-        // println!(
-        //     "Nani? {:#?}",
-        //     find_shortest_path(
-        //         &graph.nodes,
-        //         graph.edges,
-        //         HashSet::new(),
-        //         vec![grid.start()],
-        //         grid.start(),
-        //         &grid.end()
-        //     )
-        // );
-
-        // Lots of starts.
-        // Regardless of where we start, once we've reached a given node
-        // the cost to get from there to E is the same. We can cache that
-        // BETWEEN runs.
-        let starts = graph.nodes.starts();
-        let upper_limit = bounds.0 * bounds.1;
-        let shortest_from_starts = starts
-            .iter()
-            .map(|s| {
-                graph
-                    .find_shortest_path_from(s.to_owned())
-                    .map(|p| p.len())
-                    .unwrap_or(upper_limit)
-            })
-            .min();
+        let start = grid.find_start().expect("Should have a start");
+        let end = grid.find_end().expect("Should have an end");
         println!(
-            "part two: {:#?}",
-            shortest_from_starts.expect("Should have an answer")
+            "Part one: {:#?}",
+            grid.shortest_paths(vec![start], end.clone(), |c1, c2| grid
+                .one_step_condition(c1, c2))
+        );
+
+        let starts = grid.find_starts();
+        println!(
+            "Part two: {:#?}",
+            grid.shortest_paths(starts, end.clone(), |c1, c2| grid
+                .one_step_condition(c1, c2))
         );
     }
 }
 
 fn main() {
-    Day12 {}.test()
-    // Day12 {}.test_and_run()
+    Day12 {}.test_and_run()
 }
