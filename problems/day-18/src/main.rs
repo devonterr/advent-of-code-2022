ce3c86b740950cf6f5c922645eb0d5603c062fd3
Devon <axiomabsolute@gmail.com>
2023-01-02 13:45:18 -0800

    Day 18, pt 1

--- problems/day-18/src/main.rs
+++ problems/day-18/src/main.rs
@@ -1,25 +1,50 @@
+use std::collections::HashSet;
+
 use shared::{read_lines, AoCProblem, AoCSolution, Solution};
 
-struct Cube {
-    // Origin assumed to be bottom-left corner
-    origin: (usize, usize, usize),
-}
-impl TryFrom<String> for Cube {
-    type Error = String;
+// struct Cube {
+//     // Origin assumed to be bottom-left corner
+//     origin: (usize, usize, usize),
+// }
+// impl Cube {
+//     fn neighbors(&self) -> Vec<(usize, usize, usize)> {
+//         vec![
+//             (self.origin.0 + 1, self.origin.1, self.origin.2),
+//             (self.origin.0, self.origin.1 + 1, self.origin.2),
+//             (self.origin.0, self.origin.1, self.origin.2 + 1),
+//             (self.origin.0 - 1, self.origin.1, self.origin.2),
+//             (self.origin.0, self.origin.1 - 1, self.origin.2),
+//             (self.origin.0, self.origin.1, self.origin.2 - 1),
+//         ]
+//     }
+// }
+// impl TryFrom<String> for Cube {
+//     type Error = String;
 
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        let parts = value
-            .trim()
-            .split(',')
-            .map(|c| {
-                c.parse::<usize>()
-                    .expect("Should be able to parse origin coordinates")
-            })
-            .collect::<Vec<usize>>();
+//     fn try_from(value: String) -> Result<Self, Self::Error> {
+//         let parts = value
+//             .trim()
+//             .split(',')
+//             .map(|c| {
+//                 c.parse::<usize>()
+//                     .expect("Should be able to parse origin coordinates")
+//             })
+//             .collect::<Vec<usize>>();
 
-        let parts = (parts[0], parts[1], parts[2]);
-        Ok(Cube { origin: parts })
-    }
+//         let parts = (parts[0], parts[1], parts[2]);
+//         Ok(Cube { origin: parts })
+//     }
+// }
+
+fn neighbors(origin: &(usize, usize, usize)) -> Vec<(usize, usize, usize)> {
+    vec![
+        (origin.0 + 1, origin.1, origin.2),
+        (origin.0, origin.1 + 1, origin.2),
+        (origin.0, origin.1, origin.2 + 1),
+        (origin.0 - 1, origin.1, origin.2),
+        (origin.0, origin.1 - 1, origin.2),
+        (origin.0, origin.1, origin.2 - 1),
+    ]
 }
 
 struct Day18 {}
@@ -40,16 +65,22 @@ impl Solution for Day18 {
                     .trim()
                     .split(',')
                     .map(|p| {
-                        p.parse::<usize>()
+                        // Shifting everything by one because it makes it easier to deal with usize and underflow.
+                        1 + p
+                            .parse::<usize>()
                             .expect("Should be able to parse coordinate")
                     })
                     .collect::<Vec<usize>>();
                 (parts[0], parts[1], parts[2])
             })
-            .collect::<Vec<(usize, usize, usize)>>();
-        origins.sort()
+            .collect::<HashSet<(usize, usize, usize)>>();
 
-        // TODO - Loop through origins [i..] and [i+1..] and count how many are off-by-one
+        let exposed_faces = origins
+            .iter()
+            .flat_map(|o| neighbors(o).into_iter().filter(|n| !origins.contains(&n)))
+            .count();
+
+        println!("Part one: {}", exposed_faces);
     }
 }
 
