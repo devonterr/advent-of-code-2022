36f0103c349e2b15f3e7848aab3acca45c9fd39b
Devon <axiomabsolute@gmail.com>
2023-02-11 16:38:29 -0800

    Convert to graph

--- problems/day-22/src/main.rs
+++ problems/day-22/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{stdin, Read};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{stdin, Read},
+};
 
 use shared::{read_lines, AoCProblem, AoCSolution, Solution};
 
@@ -18,12 +21,12 @@ enum Orientation {
     D,
 }
 impl Orientation {
-    fn unit(&self) -> Coord {
+    fn facing(&self) -> i32 {
         match self {
-            Orientation::L => Coord { row: 0, col: -1 },
-            Orientation::R => Coord { row: 0, col: 1 },
-            Orientation::U => Coord { row: -1, col: 0 },
-            Orientation::D => Coord { row: 1, col: 0 },
+            Orientation::L => 2,
+            Orientation::R => 0,
+            Orientation::U => 3,
+            Orientation::D => 1,
         }
     }
 
@@ -42,17 +45,13 @@ impl Orientation {
     }
 }
 
-fn bounds(board: &[String]) -> (usize, usize) {
-    (board[0].chars().collect::<Vec<_>>().len(), board.len())
-}
-
 #[derive(Debug)]
 struct Instruction {
     magnitude: i32,
     rotation: Rotation,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
 struct Coord {
     row: i32,
     col: i32,
@@ -76,45 +75,6 @@ impl Coord {
         let vector = unit_vector.scale(magnitude);
         self.add(&vector)
     }
-
-    fn out_of_bounds(&self, board: &[String]) -> bool {
-        // Return true if coordinate out of bounds
-        let (width, height) = bounds(board);
-        if self.row >= height as i32 {
-            return true;
-        }
-
-        if self.col >= width as i32 {
-            return true;
-        }
-
-        false
-    }
-
-    fn wrap(&mut self, board: &[String], unit: &Coord) -> char {
-        // Check if current coordinate is out of bounds of a given board
-        // If it is, mod coordinates by board size
-        // If at an empty space, add unit and recurse
-        // If at a # stop
-        let oob = self.out_of_bounds(board);
-        if oob {
-            println!("Oob!: {:#?}", self);
-            let (width, height) = bounds(board);
-            self.row = self.row % (height as i32);
-            self.col = self.col % (width as i32);
-        } else {
-            println!("Not oob: {:#?}", self);
-        }
-        let current_tile = board[self.row as usize].chars().collect::<Vec<_>>()[self.col as usize];
-        if current_tile == '#' {
-            return current_tile;
-        }
-        if current_tile == ' ' {
-            self.add(unit);
-            return self.wrap(board, unit);
-        }
-        return current_tile;
-    }
 }
 
 #[derive(Clone, Debug)]
@@ -123,21 +83,197 @@ struct State {
     orientation: Orientation,
 }
 impl State {
-    fn new(board: &[String]) -> State {
-        let start_col = board[0].find('.').expect("First . should exist");
-        State {
-            coordinate: Coord {
+    fn new(board: &Board) -> State {
+        let mut start_col = 0;
+        while start_col < board.width {
+            let coord = Coord {
                 row: 0,
                 col: start_col as i32,
-            },
-            orientation: Orientation::R,
+            };
+            let tile = board.map.get(&coord);
+            if tile.is_some() && tile.unwrap().tile == '.' {
+                return State {
+                    coordinate: coord,
+                    orientation: Orientation::R,
+                };
+            }
+            start_col += 1;
+        }
+        panic!("Starting tile not found");
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    position: Coord,
+    tile: char,
+    neighbor_up: Option<Coord>,
+    neighbor_down: Option<Coord>,
+    neighbor_left: Option<Coord>,
+    neighbor_right: Option<Coord>,
+}
+impl Node {
+    fn new_empty(position: Coord, tile: char) -> Self {
+        Node {
+            position,
+            tile,
+            neighbor_up: None,
+            neighbor_down: None,
+            neighbor_left: None,
+            neighbor_right: None,
         }
     }
 }
 
-fn walk(board: &Vec<String>, instructions: &Vec<Instruction>) -> State {
+#[derive(Debug)]
+struct Board {
+    map: HashMap<Coord, Node>,
+    width: i32,
+    height: i32,
+}
+impl Board {
+    fn get_tile(&self, coord: &Coord) -> Option<char> {
+        self.map.get(coord).map(|n| n.tile)
+    }
+    fn try_move_one(
+        &self,
+        prev_position: Coord,
+        current_position: Coord,
+        orientation: &Orientation,
+    ) -> Coord {
+        let current_node = self
+            .map
+            .get(&current_position)
+            .expect("Current node should be in board");
+        let neighbor = match orientation {
+            Orientation::L => current_node.neighbor_left.clone(),
+            Orientation::R => current_node.neighbor_right.clone(),
+            Orientation::U => current_node.neighbor_up.clone(),
+            Orientation::D => current_node.neighbor_down.clone(),
+        }
+        .expect("Neighbor should always exist");
+
+        let neighbor_tile = self
+            .get_tile(&neighbor)
+            .expect("Neighbor tile should exist");
+
+        match neighbor_tile {
+            ' ' => self.try_move_one(prev_position, neighbor, orientation),
+            '.' => neighbor,
+            '#' => prev_position,
+            _ => panic!("Shouldn't be any other tile"),
+        }
+    }
+}
+impl From<Vec<String>> for Board {
+    fn from(lines: Vec<String>) -> Self {
+        let mut node_map = HashMap::new();
+        let mut key_set = HashSet::new();
+
+        let height = lines.len() as i32;
+        let width = lines[0].chars().collect::<Vec<_>>().len() as i32;
+
+        for row in 0..lines.len() {
+            for col in 0..lines[0].len() {
+                let coord = Coord {
+                    row: row as i32,
+                    col: col as i32,
+                };
+                let tile = lines[row].chars().collect::<Vec<_>>()[col];
+                let node = Node::new_empty(coord.clone(), tile);
+                key_set.insert(coord.clone());
+                node_map.insert(coord, node);
+            }
+        }
+
+        // Connect grid to neighbors
+        for (coord, node) in node_map.iter_mut() {
+            let upper = Coord {
+                row: coord.row - 1,
+                col: coord.col,
+            };
+            let upper_neighbor = key_set.get(&upper).map(|c| c.to_owned());
+            node.neighbor_up = upper_neighbor;
+            let lower = Coord {
+                row: coord.row + 1,
+                col: coord.col,
+            };
+            let lower_neighbor = key_set.get(&lower).map(|c| c.to_owned());
+            node.neighbor_down = lower_neighbor;
+            let left = Coord {
+                row: coord.row,
+                col: coord.col - 1,
+            };
+            let left_neighbor = key_set.get(&left).map(|c| c.to_owned());
+            node.neighbor_left = left_neighbor;
+            let right = Coord {
+                row: coord.row,
+                col: coord.col + 1,
+            };
+            let right_neighbor = key_set.get(&right).map(|c| c.to_owned());
+            node.neighbor_right = right_neighbor;
+        }
+
+        // Connect edges - Left to right
+        for row in 0..lines.len() {
+            let head_coord = Coord {
+                row: row as i32,
+                col: 0,
+            };
+            let tail_coord = Coord {
+                row: row as i32,
+                col: width - 1,
+            };
+
+            let head_node = node_map
+                .get_mut(&head_coord)
+                .expect("Head node should exist");
+            head_node.neighbor_left = Some(tail_coord.clone());
+
+            let tail_node = node_map
+                .get_mut(&tail_coord)
+                .expect("Tail node should exist");
+            tail_node.neighbor_right = Some(head_coord.clone());
+        }
+
+        // Connect edges - Up to down
+        for col in 0..width {
+            let head_coord = Coord {
+                row: 0,
+                col: col as i32,
+            };
+            let tail_coord = Coord {
+                row: height - 1,
+                col: col as i32,
+            };
+
+            let head_node = node_map
+                .get_mut(&head_coord)
+                .expect("Head node should exist");
+            head_node.neighbor_up = Some(tail_coord.clone());
+
+            let tail_node = node_map
+                .get_mut(&tail_coord)
+                .expect("Tail node should exist");
+            tail_node.neighbor_down = Some(head_coord.clone());
+        }
+
+        Board {
+            map: node_map,
+            width,
+            height,
+        }
+    }
+}
+
+fn walk(board: &Board, instructions: &Vec<Instruction>) -> State {
     // Find the startig point
     let mut current_state = State::new(board);
+
+    println!(
+        "Initial state: {:#?}\n==============================",
+        current_state
+    );
 
     // For each instruction, try and follow
     for instruction in instructions {
@@ -148,26 +284,26 @@ fn walk(board: &Vec<String>, instructions: &Vec<Instruction>) -> State {
     current_state
 }
 
-fn follow_instruction(board: &Vec<String>, state: &State, instruction: &Instruction) -> State {
-    println!("Processing instruction: {:#?} - {:#?}", instruction, state);
-    stdin().read(&mut [0]).unwrap();
-    let unit = state.orientation.unit();
+fn follow_instruction(board: &Board, state: &State, instruction: &Instruction) -> State {
+    println!("Processing instruction: {:#?} - {:#?}", state, instruction);
+    // stdin().read(&mut [0]).unwrap();
+
     let mut current_position = state.coordinate.clone();
 
     for _ in 0..instruction.magnitude {
-        // Check the board at (x,y)+unit
-        let peek_position = next_position(board, &current_position, &unit);
-        let peek_value = board[peek_position.row as usize]
-            .clone()
-            .chars()
-            .collect::<Vec<_>>()[peek_position.col as usize];
-        // If # break
-        match peek_value {
-            '#' => break,
-            _ => {
-                current_position = peek_position;
-            }
+        let new_position = board.try_move_one(
+            current_position.clone(),
+            current_position.clone(),
+            &state.orientation,
+        );
+        if current_position == new_position {
+            break;
         }
+        let tile = board.map.get(&new_position);
+        if tile.unwrap().tile == '#' {
+            break;
+        }
+        current_position = new_position;
     }
 
     // Update orientation
@@ -177,18 +313,6 @@ fn follow_instruction(board: &Vec<String>, state: &State, instruction: &Instruct
         coordinate: current_position,
         orientation,
     }
-}
-
-fn next_position(board: &Vec<String>, current_position: &Coord, unit: &Coord) -> Coord {
-    // Add unit to the current position
-    // Wrap around if necessary
-
-    let mut next_position = current_position.add(unit);
-    let next_tile = next_position.wrap(board, unit);
-    if next_tile == '#' {
-        return current_position.clone();
-    }
-    next_position
 }
 
 struct Day22 {}
@@ -236,23 +360,30 @@ impl Solution for Day22 {
             })
             .collect::<Vec<_>>();
 
-        let board = lines
+        let board: Board = lines
             .clone()
             .iter()
             .rev()
             .skip(2)
             .rev()
             .map(|x| x.to_owned())
-            .collect::<Vec<_>>();
+            .collect::<Vec<_>>()
+            .into();
 
         println!("{:#?}", board);
 
         println!("Instructions: {:#?}", instructions);
 
-        walk(&board, &instructions);
+        let final_position = walk(&board, &instructions);
+        println!(
+            "Part one: {}",
+            (1000 * (final_position.coordinate.row + 1))
+                + (4 * (final_position.coordinate.col + 1))
+                + (final_position.orientation.facing())
+        )
     }
 }
 
 fn main() {
-    Day22 {}.test()
+    Day22 {}.test_and_run()
 }
